002fc9102bb8a0ef1247512e7d518e42a8733003
Darasimi <oluwahdarasimi0108@gmail.com>
2025-05-07 06:25:29 -0700

    providers added back

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F0F4F3]`}
       >
-        <Suspense> {children}</Suspense>
+        <Suspense><Providers>{children}</Providers> </Suspense>
        
       </body>
     </html>
