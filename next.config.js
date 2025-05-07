d3cb7090599de81e0278f367a3e467fb46df7114
Darasimi <oluwahdarasimi0108@gmail.com>
2025-05-06 17:28:40 -0700

    fix deployment error

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true, // ensure App Router is recognized
+  },
+};
+
+module.exports = nextConfig;
