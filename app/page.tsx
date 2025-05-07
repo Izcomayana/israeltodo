1a380545fb11de6ab059ab34bf9ad320272c8e05
Darasimi <oluwahdarasimi0108@gmail.com>
2025-05-06 18:33:17 -0700

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import shape1 from "@/public/shape1.png";
+import undraw from "@/public/undraw.png";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="container mx-auto">
+      <Image src={shape1} alt={""} />
+      <div className="flex justify-center mx-auto mt-20">
+        <Image src={undraw} alt={""} />
+      </div>
+      
+      <section>
+        <div className="text-center my-20 mx-auto max-w-2xs">
+          <h2 className="font-bold text-lg">Gets things with TODs</h2>
+          <p className="my-5">
+            Lorem ipsum dolor sit amet consectetur. Eget sit nec et euismod. Consequat urna quam felis interdum quisque. Malesuada adipiscing tristique ut eget sed.
+          </p>
+        </div>
+      </section>
+
+      <div className="flex justify-center mx-auto max-w-md mb-20">
+        <Button className="mx-auto py-6 bg-[#50C2C9] w-full text-lg font-semibold transition-all cursor-pointer hover:bg-[#50c3c9ba]">Get Started</Button>
+      </div>
     </div>
   );
 }
