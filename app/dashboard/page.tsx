8fcf4af2af05651e065f1923a501769f787f3a4e
Darasimi <oluwahdarasimi0108@gmail.com>
2025-05-07 08:56:21 -0700

    added logout btn

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,32 +5,49 @@ import React, { useEffect, useState } from "react";
 import TaskList from "./components/tasklist/page";
 import { withAuth } from "@/lib/withAuth";
 import { auth } from "@/lib/firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
   const [userName, setUserName] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user && user.displayName) {
+      if (user?.displayName) {
         setUserName(user.displayName);
       }
     });
-
     return () => unsubscribe();
   }, []);
-  
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    router.push("/login");
+  };
+
   return (
     <section>
       <div className="relative bg-[#50C2C9] h-80">
         <div className="absolute top-0">
           <Image src={"/shapetwo.png"} width={200} height={200} alt="" />
         </div>
+
+        <div className="absolute top-4 right-4">
+          <button
+            onClick={handleLogout}
+            className="bg-white text-[#d63434] px-4 py-1 text-sm font-semibold rounded shadow"
+          >
+            Logout
+          </button>
+        </div>
+
         <div className="absolute left-1/2 top-50 -translate-x-1/2 -translate-y-1/2">
           <Image src={"/Ellipse.png"} width={100} height={100} alt="Ellipse" />
         </div>
-        <p className="absolute bottom-4 left-1/2 -translate-x-1/2 font-bold text-lg text-white">
-          {userName ? `Welcome ${userName}` : "Welcome!"} 
+
+        <p className="absolute bottom-4 left-1/2 -translate-x-1/2 font-bold text-lg text-white w-full pl-4">
+          {userName ? `Welcome ${userName}` : "Welcome!"}
         </p>
       </div>
 
@@ -44,7 +61,6 @@ const Dashboard = () => {
         </div>
 
         <h2 className="text-sm font-semibold">Task list</h2>
-
         <TaskList />
       </div>
     </section>
